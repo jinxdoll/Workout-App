5cddd4aeed90a8b8cc2e850620e01fa800ad266a
amatt <amattson84@gmail.com>
2021-04-22 10:01:13 -0700

    router for finding all workouts vis get method and adding event duration to each workout.

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,23 @@
 const db = require('../models');
 const router = require('express').Router();
+
+//get workouts
+router.get("/api/workouts", (req, res) => {
+
+    db.Workout.find({}).then(dbWorkout => {
+      
+        console.log("Here are the workouts:" + dbWorkout);
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(event => {
+                total += event.duration;
+            });
+            workout.totalDuration = total;
+
+        });
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
