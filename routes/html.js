1e4a1e5a336b6a42177b53e52470a3075edd3962
amatt <amattson84@gmail.com>
2021-04-22 10:10:03 -0700

    Router get routes from stats.

--- routes/html.js
+++ routes/html.js
@@ -1,3 +1,18 @@
 //Require path and Express 
 const router = require("express").Router();
 const path = require("path");
+
+// Router get all routes.
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+// Router get routes from exercise.
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+// Router get routes from stats.
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
