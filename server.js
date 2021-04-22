534926ab023ad3a5f03fc6ef43d2297934355a74
amatt <amattson84@gmail.com>
2021-04-22 10:14:18 -0700

    PORT set up and require Express.

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
