99d858ce5c019eccbc9cfe3ae06f3da197b70d38
amatt <amattson84@gmail.com>
2021-04-22 10:15:41 -0700

    Connection to mongoose.

--- server.js
+++ server.js
@@ -11,3 +11,14 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
