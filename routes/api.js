e0c8fb0a68771c1b8fc771c8ca6285bedf94b37c
amatt <amattson84@gmail.com>
2021-04-22 10:07:20 -0700

    Format with prettier ESLint.

--- routes/api.js
+++ routes/api.js
@@ -1,66 +1,67 @@
 const db = require('../models');
 const router = require('express').Router();
 
-//Method for getting workouts from the api. 
-router.get("/api/workouts", (req, res) => {
-
-    db.Workout.find({}).then(dbWorkout => {
-      
-        console.log("Here are the workouts:" + dbWorkout);
-        dbWorkout.forEach(workout => {
-            var total = 0;
-            workout.exercises.forEach(event => {
-                total += event.duration;
-            });
-            workout.totalDuration = total;
-
+//Method for getting workouts from the api.
+router.get('/api/workouts', (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      console.log('Here are the workouts:' + dbWorkout);
+      dbWorkout.forEach((workout) => {
+        var total = 0;
+        workout.exercises.forEach((event) => {
+          total += event.duration;
         });
+        workout.totalDuration = total;
+      });
 
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.json(err);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
     });
 });
 // Method for adding a exercise from the workouts api using the workout id.
-router.put("/api/workouts/:id", (req, res) => {
-
-    db.Workout.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-            $inc: { totalDuration: req.body.duration },
-            $push: { exercises: req.body }
-        },
-        { new: true }).then(dbWorkout => {
-            res.json(dbWorkout);
-        }).catch(err => {
-            res.json(err);
-        });
-
-});
-
-// Method for creating a new exercise and adding it to the Workout db. 
-router.post("/api/workouts", ({ body }, res) => {
-    console.log("Congrats, workout added" + body);
-
-    db.Workout.create(body).then((dbWorkout => {
-        res.json(dbWorkout);
-    })).catch(err => {
-        res.json(err);
+router.put('/api/workouts/:id', (req, res) => {
+  db.Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $inc: { totalDuration: req.body.duration },
+      $push: { exercises: req.body },
+    },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
     });
 });
 
-// Method for getting all workouts within range from the Workout db.  
-router.get("/api/workouts/range", (req, res) => {
+// Method for creating a new exercise and adding it to the Workout db.
+router.post('/api/workouts', ({ body }, res) => {
+  console.log('Congrats, workout added' + body);
 
-    db.Workout.find({}).then(dbWorkout => {
-        console.log("Here are all teh workouts within range:" + dbWorkout);     
-
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.json(err);
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
     });
-
 });
 
+// Method for getting all workouts within range from the Workout db.
+router.get('/api/workouts/range', (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      console.log('Here are all teh workouts within range:' + dbWorkout);
+
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
